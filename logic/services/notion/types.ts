de1723e3542d1b1a951c2f4ede9dae09aee8f4d8
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-07-04 11:02:14 +0900

    add: NotionAPIから型定義を抽出

---
+++ logic/services/notion/types.ts
@@ -0,0 +1,16 @@
+import type { PageObjectResponse } from "@notionhq/client";
+
+// Notionのページオブジェクトのプロパティ型を定義
+// ここでは、PageObjectResponseのpropertiesフィールドから特定のプロパティ型を抽出
+// なんでこんなことしなあかんねん
+type PageProperty = NonNullable<PageObjectResponse["properties"][string]>;
+
+type DatePropertyValue = Extract<PageProperty, { type: "date" }>;
+type SelectPropertyValue = Extract<PageProperty, { type: "select" }>;
+type MultiSelectPropertyValue = Extract<PageProperty, { type: "multi_select" }>;
+
+export type {
+    DatePropertyValue,
+    SelectPropertyValue,
+    MultiSelectPropertyValue,
+}
