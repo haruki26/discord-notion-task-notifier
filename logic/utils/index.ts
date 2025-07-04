09ac6356ba43e44d9fedb2044b5c44c87490ba97
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-07-04 14:01:26 +0900

    chore: 環境判断用の変数を設定

--- logic/utils/index.ts
+++ logic/utils/index.ts
@@ -7,6 +7,7 @@ const _getEnv = (key: string): string => {
 }
 
 export const envVars = {
+    APP_ENVIRONMENT: () => _getEnv('APP_ENVIRONMENT'),
     NOTION_API_TOKEN: () => _getEnv('NOTION_API_TOKEN'),
     NOTION_DATABASE_ID: () => _getEnv('NOTION_DATABASE_ID'),
 } as const;
