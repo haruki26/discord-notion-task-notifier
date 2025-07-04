f8933e9c9d93d5fa7ec04e23aad6df029bb6eb84
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-07-04 15:53:45 +0900

    add: discordのペイロードの型定義

---
+++ logic/services/discord/types.ts
@@ -0,0 +1,69 @@
+type DiscordWebhookPayload = {
+    content?: string;
+    username?: string;
+    avatar_url?: string;
+    tts?: boolean;
+    embeds?: DiscordEmbed[];
+    allowed_mentions?: AllowedMentions;
+    components?: unknown[];
+    attachments?: unknown[];
+}
+
+type DiscordEmbed = {
+    title?: string;
+    description?: string;
+    url?: string;
+    timestamp?: string;
+    color?: number;
+    footer?: {
+        text: string;
+        icon_url?: string;
+        proxy_icon_url?: string;
+    };
+    image?: {
+        url?: string;
+        proxy_url?: string;
+        height?: number;
+        width?: number;
+    };
+    thumbnail?: {
+        url?: string;
+        proxy_url?: string;
+        height?: number;
+        width?: number;
+    };
+    video?: {
+        url?: string;
+        proxy_url?: string;
+        height?: number;
+        width?: number;
+    };
+    provider?: {
+        name?: string;
+        url?: string;
+    };
+    author?: {
+        name?: string;
+        url?: string;
+        icon_url?: string;
+        proxy_icon_url?: string;
+    };
+    fields?: {
+        name: string;
+        value: string;
+        inline?: boolean;
+    }[];
+}
+
+type AllowedMentions = {
+    parse?: ('roles' | 'users' | 'everyone')[];
+    users?: string[];
+    roles?: string[];
+    replied_user: boolean;
+}
+
+export type {
+    DiscordWebhookPayload,
+    DiscordEmbed,
+    AllowedMentions
+}
