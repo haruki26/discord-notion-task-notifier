99d49f29c15acf8f6e7085a60077860717b482fb
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-07-05 00:11:38 +0900

    add: 通知のロジックを作成

---
+++ logic/routes/notify/index.ts
@@ -0,0 +1,30 @@
+import { Context } from "hono";
+import {
+    getAllPagesFromDatabase,
+    getAssignUsers,
+    getDueDate,
+    getTaskName,
+    sortByDueDate,
+    sortByPriority
+} from "../../services/notion";
+import { TEMPLATES } from "../../services/discord/constants";
+import { sendMessage } from "../../services/discord";
+
+export const notify = async (_: Context): Promise<void> => {
+    const pages = await getAllPagesFromDatabase();
+    const sortedPages = sortByPriority(sortByDueDate(pages).slice(0, 3));
+    const assignUsers = sortedPages.map(page => getAssignUsers(page));
+
+    const message = sortedPages.map((page, idx) => {
+        const taskName =  getTaskName(page);
+        const url = `https://www.notion.so/${page.id.replace(/-/g, "")}`;
+        const users = assignUsers[idx];
+        const due = getDueDate(page);
+        return TEMPLATES.task(taskName, url, users, due);
+    }).join("\n\n");
+
+    await sendMessage({
+        "content": `# 仕事があるお\n\n${message}`,
+        "allowed_mentions": { "parse": ["users"], "replied_user": false },
+    })
+}
