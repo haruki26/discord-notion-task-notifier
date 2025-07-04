be928672ad706c49b8d6ea03b75ca916ff45401a
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-07-04 11:04:01 +0900

    add: 環境変数取得処理を作成

---
+++ logic/utils/index.ts
@@ -0,0 +1,12 @@
+const _getEnv = (key: string): string => {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Environment variable ${key} is not set`);
+    }
+    return value;
+}
+
+export const envVars = {
+    NOTION_API_TOKEN: () => _getEnv('NOTION_API_TOKEN'),
+    NOTION_DATABASE_ID: () => _getEnv('NOTION_DATABASE_ID'),
+} as const;
