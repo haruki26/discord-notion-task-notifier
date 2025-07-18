2f210a94bbe15bbe601b12539d7cc004a7105fb1
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-07-19 03:11:51 +0900

    fix: console.logの削除

--- logic/services/discord/index.ts
+++ logic/services/discord/index.ts
@@ -6,16 +6,9 @@ import type { DiscordWebhookPayload, Ids } from "./types";
 
 const sendMessage = async (payload: DiscordWebhookPayload): Promise<void> => {
     try {
-        const response = await axios.post(envVars.DISCORD_WEBHOOK_URL, payload);
-
-        if (response.status === 204) {
-            console.log('メッセージが正常に送信されました！');
-        } else {
-            console.error(`メッセージ送信に失敗しました。ステータスコード: ${response.status}`);
-            console.error('レスポンスデータ:', response.data);
-        }
+        await axios.post(envVars.DISCORD_WEBHOOK_URL, payload);
     } catch (error) {
-        console.error('予期せぬエラーが発生しました:', error);
+        return
     }
 }
 
